a1e09080c4f366b80c8a8a32b4dc9c1c570b3afd
Anton Vinther <anto3673@mail.hansenberg.dk>
2024-02-05 08:14:39 +0100

    idk

--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ element - 1 : 1
 element - 2 : 1
 Expected Output :
 Total number of duplicate elements found in the array is : 1
-*/
 Console.WriteLine("How many elements do you want the array to be?");
 int length = Convert.ToInt32 (Console.ReadLine());
 int[] numbers = new int[length];
@@ -39,4 +38,54 @@ for (int j = 0; j < numbers.Length; j++){
     if (!duped) {
         dupes[dupes.Length + 1] = lolololol;
     }
+}
+euler 10
+*/
+List<List<int>> grid = new List<List<int>> { };
+
+List<int> line01 = new List<int> { 08, 02, 22, 97, 38, 15, 00, 40, 00, 75, 04, 05, 07, 78, 52, 12, 50, 77, 91, 08 };
+List<int> line02 = new List<int> { 49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 04, 56, 62, 00 };
+List<int> line03 = new List<int> { 81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 03, 49, 13, 36, 65 };
+List<int> line04 = new List<int> { 52, 70, 95, 23, 04, 60, 11, 42, 69, 24, 68, 56, 01, 32, 56, 71, 37, 02, 36, 91 };
+List<int> line05 = new List<int> { 22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80 };
+List<int> line06 = new List<int> { 24, 47, 32, 60, 99, 03, 45, 02, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50 };
+List<int> line07 = new List<int> { 32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70 };
+List<int> line08 = new List<int> { 67, 26, 20, 68, 02, 62, 12, 20, 95, 63, 94, 39, 63, 08, 40, 91, 66, 49, 94, 21 };
+List<int> line09 = new List<int> { 24, 55, 58, 05, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72 };
+List<int> line10 = new List<int> { 21, 36, 23, 09, 75, 00, 76, 44, 20, 45, 35, 14, 00, 61, 33, 97, 34, 31, 33, 95 };
+List<int> line11 = new List<int> { 78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 03, 80, 04, 62, 16, 14, 09, 53, 56, 92 };
+List<int> line12 = new List<int> { 16, 39, 05, 42, 96, 35, 31, 47, 55, 58, 88, 24, 00, 17, 54, 24, 36, 29, 85, 57 };
+List<int> line13 = new List<int> { 86, 56, 00, 48, 35, 71, 89, 07, 05, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58 };
+List<int> line14 = new List<int> { 19, 80, 81, 68, 05, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 04, 89, 55, 40 };
+List<int> line15 = new List<int> { 04, 52, 08, 83, 97, 35, 99, 16, 07, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66 };
+List<int> line16 = new List<int> { 88, 36, 68, 87, 57, 62, 20, 72, 03, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69 };
+List<int> line17 = new List<int> { 04, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 08, 46, 29, 32, 40, 62, 76, 36 };
+List<int> line18 = new List<int> { 20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 04, 36, 16 };
+List<int> line19 = new List<int> { 20, 73, 35, 29, 78, 31, 90, 01, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 05, 54 };
+List<int> line20 = new List<int> { 01, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 01, 89, 19, 67, 48 };
+
+grid.Add(line01);
+grid.Add(line02);
+grid.Add(line03);
+grid.Add(line04);
+grid.Add(line05);
+grid.Add(line06);
+grid.Add(line07);
+grid.Add(line08);
+grid.Add(line09);
+grid.Add(line10);
+grid.Add(line11);
+grid.Add(line12);
+grid.Add(line13);
+grid.Add(line14);
+grid.Add(line15);
+grid.Add(line16);
+grid.Add(line17);
+grid.Add(line18);
+grid.Add(line19);
+grid.Add(line20);
+
+for (int i = 0; i < 20; i++)
+{
+    Console.WriteLine(grid[i]);
 }
